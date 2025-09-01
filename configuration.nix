c352edd87bbf642a4cbdd0e379e5c5ac50181a36
vibewill2 <vibewill@gmail.com>
2025-09-01 11:08:45 -0300

    kitty

--- configuration.nix
+++ configuration.nix
@@ -88,7 +88,7 @@
     # vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     # wget
     waybar
-    alacritty
+    kitty
     rofi-wayland
     swww
     google-chrome
