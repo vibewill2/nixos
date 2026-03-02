f4cce3198eacf23d1239cd9ba87af91f72cd8825
vibewill2 <vibewill@gmail.com>
2026-03-02 10:48:14 -0300

    Add files via upload

--- sway/status.sh
+++ sway/status.sh
@@ -1,22 +1,20 @@
 #!/bin/sh
 
-# Detecta a interface de rede ativa automaticamente (ex: enp3s0 ou wlan0)
+# Detecta a interface de rede ativa
 INTERFACE=$(ip route | grep default | awk '{print $5}' | head -n1)
 
 while true; do
-  # --- CÁLCULO DE REDE (Diferença de 1 segundo) ---
-  # Lê os bytes recebidos e enviados
+  # --- CÁLCULO DE REDE ---
   R1=$(cat /sys/class/net/$INTERFACE/statistics/rx_bytes)
   T1=$(cat /sys/class/net/$INTERFACE/statistics/tx_bytes)
   sleep 1
   R2=$(cat /sys/class/net/$INTERFACE/statistics/rx_bytes)
   T2=$(cat /sys/class/net/$INTERFACE/statistics/tx_bytes)
 
-  # Converte a diferença para KB/s
   RX=$(( (R2 - R1) / 1024 ))
   TX=$(( (T2 - T1) / 1024 ))
 
-  # --- CPU (Carga média de 1 min via /proc para evitar erro de tradução) ---
+  # --- CPU ---
   CPU=$(cut -d' ' -f1 /proc/loadavg)
 
   # --- MEMÓRIA ---
@@ -29,7 +27,7 @@ while true; do
   # --- DATA ---
   DATE=$(date +'%d/%m/%Y %H:%M')
 
-  # SAÍDA: Um único ícone para Down e Up 🌐
-  # Formato: CPU | MEM | REDE (D/U) | VOL | DATA
-  echo " $CPU | 💾 $MEM | 🌐 ${RX}K↓ ${TX}K↑ |  $VOL |  $DATE"
+  # --- SAÍDA FINAL COM TODAS AS CORES ---
+  # CPU: Ciano | MEM: Verde | REDE: Amarelo | VOL: Magenta | DATA: Laranja/Ouro
+  echo "<span color='#00ffff'></span> $CPU | <span color='#00ff00'>💾</span> $MEM | <span color='#ffff00'>🌐</span> ${RX}K↓ ${TX}K↑ | <span color='#ff00ff'></span> $VOL | <span color='#ffaa00'></span> $DATE"
 done
